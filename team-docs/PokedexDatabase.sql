70b4afaa72748fbff7257a19c02646609fa8bb0f
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-08-05 10:54:02 -0700

    Rewrote the PokedexDatabase.sql
    When this script runs, it deletes the existing PokedexDB and recreates it. Afterwards, it creates the tables and adds data to them.

--- team-docs/PokedexDatabase.sql
+++ team-docs/PokedexDatabase.sql
@@ -1,73 +1,52 @@
--- If you want to create the database for yourself then you can uncomment this
--- everything below this line section by section and execute it.
+USE master;
+GO
 
--- Create DATABASE PokedexDB;
+DROP DATABASE IF EXISTS PokedexDB;
+GO
 
-USE PokedexDB; 
-SELECT * FROM dbo.Pokemon;
+CREATE DATABASE PokedexDB;
+GO
 
---Create TABLE Pokemon ( 
-	--PokemonId INT PRIMARY KEY, 
-	--Name VARCHAR(30), 
-	--HP INT, 
-	--Defense INT, 
-	--Attack INT,
-	--SpecialAttack INT, 
-	--SpecialDefense INT,
-	--Speed INT, 
-	--PokemonType1 VARCHAR(35), 
-	--PokemonType2 VARCHAR(35) NULL,  -- optionally NULL ! not all Pokemon have two types. 
---);
+USE PokedexDB;
+GO
 
---INSERT INTO Pokemon (PokemonId, Name, HP, Defense, 
---Attack, SpecialAttack, SpecialDefense, Speed, PokemonType1, PokemonType2)
+CREATE TABLE Pokemon (
+  PokemonId INT PRIMARY KEY IDENTITY(1, 1),
+  Name VARCHAR(35) NOT NULL,
+  HP INT NOT NULL,
+  Defense INT NOT NULL,
+  Attack INT NOT NULL,
+  SpecialAttack INT NOT NULL,
+  SpecialDefense INT NOT NULL,
+  Speed INT,
+  PokemonType1 VARCHAR(35) NOT NULL,
+  PokemonType2 VARCHAR(35)
+);
 
---VALUES 
--- GRASS/POISON TYPES 
---(1, 'Bulbasaur', 45, 49, 49, 65, 65, 45, 'Grass', 'Poison'),
---(2, 'Ivysaur', 60, 62, 63, 80, 80, 60, 'Grass', 'Poison'),
---(3, 'Venusaur', 80, 82, 83, 100, 100, 80, 'Grass', 'Poison'),
+INSERT INTO Pokemon (Name, HP, Defense, Attack, SpecialAttack, SpecialDefense, Speed, PokemonType1, PokemonType2)
+VALUES
+-- GRASS/POISON TYPES
+('Bulbasaur', 45, 49, 49, 65, 65, 45, 'Grass', 'Poison'),
+('Ivysaur', 60, 62, 63, 80, 80, 60, 'Grass', 'Poison'),
+('Venusaur', 80, 82, 83, 100, 100, 80, 'Grass', 'Poison'),
 
--- FIRE/FLYING TYPES  
---(4, 'Charmander', 39, 52, 43, 60, 50, 65, 'Fire', NULL),
---(5, 'Charmeleon', 58, 64, 58, 80, 65, 80, 'Fire', NULL),
---(6, 'Charizard', 78, 84, 78, 109, 85, 100, 'Fire', 'Flying'),
+-- FIRE/FLYING TYPES
+('Charmander', 39, 52, 43, 60, 50, 65, 'Fire', NULL),
+('Charmeleon', 58, 64, 58, 80, 65, 80, 'Fire', NULL),
+('Charizard', 78, 84, 78, 109, 85, 100, 'Fire', 'Flying'),
 
 -- WATER TYPES
---(7, 'Squirtle', 44, 48, 65, 50, 64, 43, 'Water', NULL),
---(8, 'Wartortle', 59, 63, 80, 65, 80, 58, 'Water', NULL),
---(9, 'Blastoise', 79, 83, 100, 85, 105, 78, 'Water', NULL),
+('Squirtle', 44, 48, 65, 50, 64, 43, 'Water', NULL),
+('Wartortle', 59, 63, 80, 65, 80, 58, 'Water', NULL),
+('Blastoise', 79, 83, 100, 85, 105, 78, 'Water', NULL),
 
 -- BUG/POISON/FLYING TYPES
---(10, 'Caterpie', 45, 30, 35, 20, 20, 45, 'Bug', NULL),
---(11, 'Metapod', 50, 20, 55, 25, 25, 30, 'Bug', NULL),
---(12, 'Butterfree', 60, 45, 50, 90, 80, 70, 'Bug', 'Flying'),
---(13, 'Weedle', 40, 35, 30, 20, 20, 50, 'Bug', 'Poison'),
---(14, 'Kakuna', 45, 25, 50, 25, 25, 35, 'Bug', 'Poison'),
---(15, 'Beedrill', 65, 90, 40, 45, 80, 75, 'Bug', 'Poison');
+('Caterpie', 45, 30, 35, 20, 20, 45, 'Bug', NULL),
+('Metapod', 50, 20, 55, 25, 25, 30, 'Bug', NULL),
+('Butterfree', 60, 45, 50, 90, 80, 70, 'Bug', 'Flying'),
+('Weedle', 40, 35, 30, 20, 20, 50, 'Bug', 'Poison'),
+('Kakuna', 45, 25, 50, 25, 25, 35, 'Bug', 'Poison'),
+('Beedrill', 65, 90, 40, 45, 80, 75, 'Bug', 'Poison');
 
-SELECT FORMAT(PokemonID, '0000') AS FormattedID, Name
+SELECT FORMAT(PokemonId, '0000') AS FormattedID, Name
 FROM Pokemon;
------------------------------------------------------
--- Below ALTERS columns so that the following are required and CANNOT be NULL --
------------------------------------------------------
---ALTER TABLE Pokemon
---ALTER COLUMN Name VARCHAR(35) NOT NULL;
-
---ALTER TABLE Pokemon
---ALTER COLUMN HP INT NOT NULL;
-
---ALTER TABLE Pokemon
---ALTER COLUMN Defense INT NOT NULL;
-
---ALTER TABLE Pokemon
---ALTER COLUMN Attack INT NOT NULL;
-
---ALTER TABLE Pokemon
---ALTER COLUMN SpecialAttack INT NOT NULL;
-
---ALTER TABLE Pokemon
---ALTER COLUMN SpecialDefense INT NOT NULL;
-
---ALTER TABLE Pokemon
---ALTER COLUMN PokemonType1 VARCHAR(35) NOT NULL;
