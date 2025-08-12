3bfbdd744d005f32ca552b87239567f095731c1b
Adelisef33 <AdeliseFerris@gmail.com>
2025-08-11 22:21:13 -0700

    Created the pokemon class and modified the content to properly assess all data the pokemon can have

---
+++ Pokedex/Pokemon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex
+{
+    public class Pokemon
+    {
+        // <summary>    
+        // Represents a Pokemon's number in the pokedex.
+        // </summary>
+        public int PokemonID { get; set; }
+
+        /// <summary>
+        /// Refers to the name of the Pokemon.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The total amount of health points (HP) a Pokemon has.
+        /// </summary>
+        public int HP { get; set; }
+
+        /// <summary>   
+        /// Determines the power of a Pokemon's attack.
+        /// </summary>
+
+        public byte Attack { get; set; }
+
+        /// <summary>
+        /// Determines the amount of damage a Pokemon will resist.
+        /// </summary>
+
+        public byte Defense { get; set; }
+
+        /// <summary>
+        /// Determines the power of a Pokemon's special attack.
+        /// </summary>
+
+        public byte SpAttack { get; set; }
+
+        /// <summary>
+        /// Determines how much damage a Pokemon will resist from special attacks.  
+        /// </summary>
+        public byte SpDefense { get; set; }
+
+        /// <summary>
+        /// Determines the speed of a Pokemon. (This is important for determining which pokemon attacks first in battle.)
+        /// </summary>
+        public byte Speed { get; set; }
+
+        /// <summary>
+        /// The Pokmon's primary type.
+        /// </summary>
+        public required string PokemonType1 { get; set; }
+
+        /// <summary>
+        /// The Pokemon's secondary type, if applicable.
+        /// </summary>
+        public string? PokemonType2 { get; set; }
+
+    }
+}
