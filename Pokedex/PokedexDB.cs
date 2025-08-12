245aa354cb8b68a44043bd41d09a145558721721
VacaSama <klc_moore@outlook.com>
2025-08-12 10:46:04 -0700

    Notes added for Team member -Kourtnie on Database and Database class update.

--- Pokedex/PokedexDB.cs
+++ Pokedex/PokedexDB.cs
@@ -44,6 +44,24 @@ namespace Pokedex
                 MessageBox.Show($"Failed to connect to database:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+        // In the database class we now need to store users collected pokemon 
+        
+        public void CollectedPokemon()
+        {
+            // This method will be used to store the user's collected pokemon in the database
+            // I will need to create a table in the database:
+            // to store the user's collected pokemon 
+            // The table will have the following columns:
+            // 1. PokemonID (int, primary key)
+            // 2. UserID (int, foreign key)
+            // 3. PokemonName (string)
+            // 4. PokemonType1 (string)
+            // 5. PokemonType2 (string, nullable)
+            // ^^ can I collectively get the stats from the previous table in DB?
+            // without doing it all one at a time?
+
+        }
+
     }
 }
 #region Additional Information/Dev Notes
