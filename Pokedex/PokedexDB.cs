75547ecb71f89bba81749a6f012c1fa6132f9d53
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-08-12 09:09:28 -0700

    Removed uneeded "using" keywords and used the shorthand namespace syntax.

--- Pokedex/PokedexDB.cs
+++ Pokedex/PokedexDB.cs
@@ -1,45 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using Microsoft.Data.SqlClient;
 
-namespace Pokedex
+namespace Pokedex;
+
+/// <summary>
+///  PokedexDB is a class that connects to the Pokedex database.
+/// </summary>
+public class PokedexDB
 {
-    /// <summary>
-    ///  PokedexDB is a class that connects to the Pokedex database.
-    /// </summary>
-    public class PokedexDB
+    // make the connection string a constant and change the data source to localhost 
+    // localhost made 
+    string connectionString = "Data Source=localhost;Initial Catalog=PokedexDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    // this is a class that will connect the Pokedex to the database
+    public PokedexDB()
     {
-        // make the connection string a constant and change the data source to localhost 
-        // localhost made 
-        string connectionString = "Data Source=localhost;Initial Catalog=PokedexDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
-        // this is a class that will connect the Pokedex to the database
-        public PokedexDB()
+        // test query to see if the connection is successful
+        try
         {
-            // test query to see if the connection is successful
-            try
-            {
-                using SqlConnection connection = new SqlConnection(connectionString);
-                connection.Open();
-                // If connection opens successfully, show success message
-                                            // Showed Message, MessageBox Title, MessageBox Buttons, and MessageBox Icon
-                MessageBox.Show("Database connection, complete!", "Connection Sucessful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            // If connection opens successfully, show success message
+                                        // Showed Message, MessageBox Title, MessageBox Buttons, and MessageBox Icon
+            MessageBox.Show("Database connection, complete!", "Connection Sucessful", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                // execute a test query
-                string query = "SELECT * FROM Pokemon";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                // If connection fails, show error message
-                MessageBox.Show($"Failed to connect to database:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            // execute a test query
+            string query = "SELECT * FROM Pokemon";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            // If connection fails, show error message
+            MessageBox.Show($"Failed to connect to database:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
