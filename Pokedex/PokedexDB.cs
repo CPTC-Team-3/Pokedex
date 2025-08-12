1c624ac84146e2250daf5e0045e37d1b5a32a6d0
VacaSama <klc_moore@outlook.com>
2025-08-12 12:21:40 -0700

    Database string update kourtnie (#17)
    * Connection String updated to the (localdb), should now work for all team memebers
    
    * Notes added for Team member -Kourtnie on Database and Database class update.
    
    * fixing merge errror

--- Pokedex/PokedexDB.cs
+++ Pokedex/PokedexDB.cs
@@ -7,15 +7,22 @@ namespace Pokedex;
 /// </summary>
 public class PokedexDB
 {
-    // make the connection string a constant and change the data source to localhost 
-    // localhost made 
-    string connectionString = "Data Source=localhost;Initial Catalog=PokedexDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    /// <summary>
+    ///  PokedexDB is a class that connects to the Pokedex database.
+    ///  This is the previous connectionString that was used:       
+    /// // string connectionString = "Data Source=localhost;Initial Catalog=PokedexDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";    /// </summary>
+    public class PokedexDB
+    {
+        // (localdb) connection made 
+      string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PokedexDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+      
 
     // this is a class that will connect the Pokedex to the database
     public PokedexDB()
     {
         // test query to see if the connection is successful
         try
+
         {
             using SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
@@ -33,6 +40,24 @@ public class PokedexDB
             // If connection fails, show error message
             MessageBox.Show($"Failed to connect to database:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
+        // In the database class we now need to store users collected pokemon 
+        
+        public void CollectedPokemon()
+        {
+            // This method will be used to store the user's collected pokemon in the database
+            // I will need to create a table in the database:
+            // to store the user's collected pokemon 
+            // The table will have the following columns:
+            // 1. PokemonID (int, primary key)
+            // 2. UserID (int, foreign key)
+            // 3. PokemonName (string)
+            // 4. PokemonType1 (string)
+            // 5. PokemonType2 (string, nullable)
+            // ^^ can I collectively get the stats from the previous table in DB?
+            // without doing it all one at a time?
+
+        }
+
     }
 }
 #region Additional Information/Dev Notes
